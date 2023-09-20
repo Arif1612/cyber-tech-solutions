92790ce6af50c2d6b6e5a81a536927886a007236
Arif <arifulislam161148@gmail.com>
2023-09-20 21:30:41 +0600

    tailwind css installed and extension daisyUI installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true,node:true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -12,6 +12,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/jsx-uses-react": "error",   
+    "react/jsx-uses-vars": "error", 
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
